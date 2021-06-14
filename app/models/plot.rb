ab817213d60330592b9f61764be5d98e1e485f64
Zach T <ztrokey@gmail.com>
2021-06-14 11:50:55 -0600

    remove unnecessary test setup data

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -5,5 +5,7 @@ class Plot < ApplicationRecord
 
   def plant_names
     plants.pluck(:name)
+    #I didn't use this method but my thoughts were to try to use it
+    #to clean up the double iteration on the plots index page
   end
 end
