93071e7927186eb5ab33f347431f6753e824314d
Zach T <ztrokey@gmail.com>
2021-06-14 11:13:10 -0600

    complete US3

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,6 +2,10 @@ class Garden < ApplicationRecord
   has_many :plots
 
   def fast_plants
-    plots.joins(:plants).where('days_to_harvest < 100').distinct.pluck(:name)
+    plots
+      .joins(:plants)
+      .where('days_to_harvest < 100')
+      .distinct
+      .pluck(:name)
   end
 end
