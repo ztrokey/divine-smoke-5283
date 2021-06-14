d0a24b6ccf33cb168a751a743d995395f5168f81
Zach T <ztrokey@gmail.com>
2021-06-14 11:42:48 -0600

    add plant_names method to plots

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -2,4 +2,8 @@ class Plot < ApplicationRecord
   belongs_to :garden
   has_many :plot_plants
   has_many :plants, through: :plot_plants
+
+  def plant_names
+    plants.pluck(:name)
+  end
 end
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe Plot do
     it {should have_many :plot_plants}
     it {should have_many(:plants).through(:plot_plants)}
   end
+
+  describe 'plants' do
+    it 'lists all the plant names for a plot' do
+      @garden1 = Garden.create!(name: 'Garden #1', organic: true)
+
+      @plot1 = Plot.create!(number: 1, size: 'large', direction: 'West', garden_id: @garden1.id)
+      @plot2 = Plot.create!(number: 2, size: 'small', direction: 'East', garden_id: @garden1.id)
+
+      @plant1 = Plant.create!(name: 'potato', description: 'big', days_to_harvest: 15)
+      @plant2 = Plant.create!(name: 'sunflower', description: 'biggerer', days_to_harvest: 25)
+      @plant3 = Plant.create!(name: 'something else', description: 'its something', days_to_harvest: 50)
+      @plant4 = Plant.create!(name: 'Long plant', description: 'it takes a long time', days_to_harvest: 120)
+
+      @pp1 = PlotPlant.create!(plot_id: @plot1.id, plant_id: @plant1.id)
+      @pp2 = PlotPlant.create!(plot_id: @plot2.id, plant_id: @plant2.id)
+      @pp3 = PlotPlant.create!(plot_id: @plot1.id, plant_id: @plant3.id)
+      @pp4 = PlotPlant.create!(plot_id: @plot1.id, plant_id: @plant4.id)
+      @pp5 = PlotPlant.create!(plot_id: @plot2.id, plant_id: @plant1.id)
+
+      expect(@plot1.plant_names).to eq(([@plant1.name, @plant3.name, @plant4.name]))
+    end
+  end
 end
